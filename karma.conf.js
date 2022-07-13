b6ae006d621be54b000794ff12942819d61c6a63
bhavani-velivala-sn <bhavani.velivala@servcienow.com>
2022-07-14 02:45:12 +0530

    changes for test summaries

--- karma.conf.js
+++ karma.conf.js
@@ -44,14 +44,14 @@ module.exports = function (config) {
 
     // the default configuration
     junitReporter: {
-      outputDir: '', // results will be saved as $outputDir/$browserName.xml
-      outputFile: undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
-      suite: '', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: true, // add browser name to report and classes names
-      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
-      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
-      properties: {}, // key value pair of properties to add to the <properties> section of the report
-      xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
+      outputDir: 'reports', // results will be saved as $outputDir/$browserName.xml
+      outputFile: 'testResults.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
+      //suite: '', // suite will become the package name attribute in xml testsuite element
+      useBrowserName: false, // add browser name to report and classes names
+      //nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
+      //classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
+      //properties: {}, // key value pair of properties to add to the <properties> section of the report
+      //xmlVersion: null // use '1' if reporting to be per SonarQube 6.2 XML format
     }
   });
 };
